14023ba60ffe7261d5867c9edda3c258a65c6592
miracleXH <h1047741619@163.com>
2021-12-17 13:32:21 +0800

    12171331

--- src/component/nav.js
+++ src/component/nav.js
@@ -6,12 +6,12 @@ class Nav extends React.Component{
     render() {
 		return (
             <div className="nav">
-                <div className="nav_logo"><img/></div>
+                <div className="nav_logo">Hacker Valley</div>
                 <div className="nav_hackathon">黑客松</div>
                 <div className="nav_projects">项目展示</div>
                 <div className="nav_search"></div> {/* 搜索框 */} 
                 <div className="nav_address">address</div>{/* 用户钱包地址或用户id */} 
-                <div className="nav_img_mine"><img/></div>
+                <div className="nav_img_mine"><img alt=""/></div>
                 <div className="nav_mine">我的</div>{/* 跳转到账户管理 */} 
             </div>
 		);
